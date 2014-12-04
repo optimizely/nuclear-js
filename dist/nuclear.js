33292db6fed117ba63c0bfab65c81fcd1914a898
jordangarcia <jordan.garcia@gmail.com>
2014-12-03 22:01:49 -0800

    Fix logging issue in node + bump

--- dist/nuclear.js
+++ dist/nuclear.js
@@ -790,22 +790,27 @@ return /******/ (function(modules) { // webpackBootstrap
 	 * Wraps a Reactor.react invocation in a console.group
 	*/
 	exports.dispatchStart = function(type, payload) {
-	  console.groupCollapsed('Dispatch: %s', type)
-	  console.group('payload')
-	  console.log(payload)
-	  console.groupEnd()
+	  if (console.group) {
+	    console.groupCollapsed('Dispatch: %s', type)
+	    console.group('payload')
+	    console.log(payload)
+	    console.groupEnd()
+	  }
 	}
 
 	exports.coreReact = function(id, before, after) {
-	  if (before !== after) {
-	    console.log('Core changed: ' + id)
+	  if (console.group) {
+	    if (before !== after) {
+	      console.log('Core changed: ' + id)
+	    }
 	  }
 	}
 
 	exports.dispatchEnd = function(state) {
-	  console.log('Dispatch done, new state: ', state.toJS())
-
-	  console.groupEnd()
+	  if (console.group) {
+	    console.log('Dispatch done, new state: ', state.toJS())
+	    console.groupEnd()
+	  }
 	}
 
 
