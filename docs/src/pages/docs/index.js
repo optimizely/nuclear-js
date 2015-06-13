fd74a029dc2c9bbc81f6aefc77c0343aa7ab9726
jordangarcia <jordan.garcia@gmail.com>
2015-06-12 19:01:36 -0700

    Fix /docs redirect

--- docs/src/pages/docs/index.js
+++ docs/src/pages/docs/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Redirect from '../../layouts/redirect'
+import { BASE_URI } from '../../globals'
 
 export default React.createClass({
   render() {
-    return <Redirect to="/" />
+    return <Redirect to={BASE_URI} />
   }
 })
