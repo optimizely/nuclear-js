aed09b018340642ad132fea5e962c689c57d2253
jordangarcia <jordan.garcia@gmail.com>
2015-07-13 12:26:52 -0700

    Docs: update mobile navbar

--- docs/src/components/nav.js
+++ docs/src/components/nav.js
@@ -8,15 +8,25 @@ function urlize(uri) {
 export default React.createClass({
   render() {
     const logo = this.props.includeLogo
-      ? <a href={BASE_URL} className="brand-logo hide-on-med-and-down">NuclearJS</a>
+      ? <a href={BASE_URL} className="brand-logo">NuclearJS</a>
+      : null
+    const homeLink = this.props.includeLogo
+      ? <li className="hide-on-large-only"><a href={urlize("")}>Home</a></li>
       : null
 
     return <div className="navbar-fixed">
       <nav className="nav">
-        <div className="nav-wrapper">
+        <div className="hide-on-large-only">
+          <ul className="right">
+            {homeLink}
+            <li><a href={urlize("docs/01-getting-started.html")}>Docs</a></li>
+            <li><a href={urlize("docs/07-api.html")}>API</a></li>
+            <li><a href="https://github.com/optimizely/nuclear-js">Github</a></li>
+          </ul>
+        </div>
+        <div className="nav-wrapper hide-on-med-and-down">
           {logo}
           <ul className="right">
-            <li className="hide-on-large-only"><a href={urlize("")}>Home</a></li>
             <li><a href={urlize("docs/01-getting-started.html")}>Docs</a></li>
             <li><a href={urlize("docs/07-api.html")}>API</a></li>
             <li><a href="https://github.com/optimizely/nuclear-js">Github</a></li>
